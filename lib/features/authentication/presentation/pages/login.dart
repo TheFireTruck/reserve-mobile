5b14cd8a3af6a886652836a72d20a96dea8daa93
Aaron Djangmah <djangsiness01@gmail.com>
2024-03-10 20:46:19 +0000

    development

--- lib/features/authentication/presentation/pages/login.dart
+++ lib/features/authentication/presentation/pages/login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:reserve_io/features/home/presentation/pages/homepage.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -11,11 +13,191 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: [
-          Image.asset('assets/image.png'),
-        ],
+      backgroundColor: Colors.white,
+      persistentFooterButtons: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Create an account?'),
+            TextButton(onPressed: () {}, child: const Text('Sign Up'))
+          ],
+        )
+      ],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Image.asset('assets/Image1.png'),
+              Text(
+                'OfficeReserve',
+                style: GoogleFonts.openSans(
+                    fontSize: 30, fontWeight: FontWeight.w500),
+              ),
+              Text(
+                'Reserve office resources efficiently',
+                style: GoogleFonts.openSans(
+                    fontSize: 16, fontWeight: FontWeight.w300),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                      width: 350,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          boxShadow: const [BoxShadow(blurRadius: 2)],
+                          borderRadius: BorderRadius.circular(20)),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: 'Company ID',
+                          border: InputBorder.none,
+                          prefixIcon: Icon(Icons.house),
+                        ),
+                      ))
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                      width: 350,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          boxShadow: const [BoxShadow(blurRadius: 2)],
+                          borderRadius: BorderRadius.circular(20)),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                            hintText: 'Email',
+                            border: InputBorder.none,
+                            prefixIcon: Icon(Icons.email)),
+                      ))
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 350,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        boxShadow: const [BoxShadow(blurRadius: 2)],
+                        borderRadius: BorderRadius.circular(20)),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                          hintText: 'Password',
+                          border: InputBorder.none,
+                          prefixIcon: Icon(Icons.lock)),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 190),
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text('Forgot Password',
+                      style: GoogleFonts.openSans(color: Colors.black)),
+                ),
+              ),
+              GestureDetector(
+                onTap: () => Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (context) => Homepage()),
+                ),
+                child: Container(
+                  width: 300,
+                  height: 60,
+                  decoration: BoxDecoration(
+                      color: Colors.orange,
+                      borderRadius: BorderRadius.circular(50)),
+                  child: Center(
+                    child: Text(
+                      'Sign In',
+                      style: GoogleFonts.openSans(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const Row(
+                children: [
+                  Expanded(
+                    child: Divider(
+                      indent: 30,
+                      endIndent: 20,
+                    ),
+                  ),
+                  Text('Or'),
+                  Expanded(
+                    child: Divider(
+                      indent: 20,
+                      endIndent: 10,
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 150,
+                    height: 60,
+                    decoration: BoxDecoration(
+                        color: Colors.orange,
+                        borderRadius: BorderRadius.circular(50)),
+                    child: Center(
+                      child: Text(
+                        'Google',
+                        style: GoogleFonts.openSans(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 50,
+                  ),
+                  Container(
+                    width: 150,
+                    height: 60,
+                    decoration: BoxDecoration(
+                        color: Colors.orange,
+                        borderRadius: BorderRadius.circular(50)),
+                    child: Center(
+                      child: Text(
+                        'Facebook',
+                        style: GoogleFonts.openSans(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.white),
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/features/home/presentation/pages/homepage.dart
+++ lib/features/home/presentation/pages/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -10,6 +11,132 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(),body: Column(children: [],),);
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.laptop), label: 'My Desk'),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Menu'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.credit_card), label: 'Orders'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile')
+          ],
+          type: BottomNavigationBarType.fixed,
+        ),
+        appBar: AppBar(
+          title: Center(
+            child: Container(
+              width: 350,
+              height: 50,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(50),
+                  boxShadow: const [BoxShadow(blurRadius: 1.5)]),
+              child: Center(
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      prefixIcon: Icon(Icons.search),
+                      hintText: 'Search Resource',
+                      border: InputBorder.none),
+                ),
+              ),
+            ),
+          ),
+          bottom: const TabBar(tabs: [
+            Tab(
+              text: 'Meeting',
+            ),
+            Tab(
+              text: 'Workstation',
+            ),
+            Tab(
+              text: 'Order Food',
+            )
+          ]),
+        ),
+        body: TabBarView(children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    width: 190,
+                    height: 170,
+                    decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.circular(20)),
+                  ),
+                  Container(
+                    width: 150,
+                    height: 170,
+                    decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.circular(20)),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    width: 150,
+                    height: 170,
+                    decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.circular(20)),
+                  ),
+                  Container(
+                    width: 190,
+                    height: 170,
+                    decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.circular(20)),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30),
+                    child: Container(
+                      width: 150,
+                      height: 170,
+                      decoration: BoxDecoration(
+                          color: Colors.amber,
+                          borderRadius: BorderRadius.circular(20)),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 90,
+                  ),
+                  Container(
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.add),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+          const Column(
+            children: [Text('data')],
+          ),
+          const Column(
+            children: [Text('data')],
+          )
+        ]),
+      ),
+    );
   }
 }
